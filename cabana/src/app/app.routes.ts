b1cb4f233ac28ff155a12fd0d2a5aa7704521400
phuongltlABC <157083799+phuongltlABC@users.noreply.github.com>
2025-03-28 11:27:53 +0700

    update

--- cabana/src/app/app.routes.ts
+++ cabana/src/app/app.routes.ts
@@ -1,9 +1,5 @@
 import { Routes } from '@angular/router';
-import { OrdersSummaryComponent } from './pages/orders-summary/orders-summary.component';
-import { OrderDetailsComponent } from './pages/order-details/order-details.component';
 
 export const routes: Routes = [
-  { path: '', component: OrdersSummaryComponent },
-  { path: 'orders/:orderID', component: OrderDetailsComponent },
   { path: '**', redirectTo: '' }
 ];
